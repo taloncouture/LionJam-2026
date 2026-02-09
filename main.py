5233855e97596d5d62f6a527698978d08d704461
Talon Couture <talon.m.couture@gmail.com>
2026-02-09 18:27:28 -0500

    Can now place tiles

--- main.py
+++ main.py
@@ -14,50 +14,11 @@ game_surface = pygame.Surface((WIDTH, HEIGHT))
 
 
 
-tiles_ground = [[' ', ' ', ' ', ' ', '0', '0', '0', '0', ' ', ' ', '0', '0', '0', ' ', ' '],
-         [' ', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', ' ', ' '],
-         [' ', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', ' '],
-         ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', ' '],
-         ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', ' ', ' '],
-         ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', ' '],
-         [' ', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', ' '],
-         ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
-         ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
-         ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
-         ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
-         [' ', '0', '0', '0', '0', '0', '0', '0', '0', '0', ' ', '0', '0', '0', ' '],
-         [' ', '0', '0', '0', ' ', ' ', '0', '0', '0', '0', ' ', '0', '0', '0', ' '],
-         [' ', ' ', '0', '0', ' ', '0', '0', '0', '0', '0', ' ', '0', '0', '0', ' '],
-         [' ', ' ', ' ', ' ', ' ', ' ', ' ', '0', '0', ' ', ' ', ' ', ' ', ' ', ' ']]
-
-tiles2 = [[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-         [' ', ' ', '!', '~', '~', 't', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-         [' ', ' ', '~', '~', '~', 't', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-         [' ', ' ', '~', '~', '~', ' ', ' ', ' ', ' ', 't', ' ', ' ', ' ', ' ', ' '],
-         [' ', ' ', '@', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-         [' ', ' ', ' ', ' ', 'f', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-         [' ', ' ', ' ', ' ', ' ', 't', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 't', ' ', ' ', ' ', ' ', ' '],
-         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']]
-
-
 selected_x = 0
 selected_y = 0
 
 
 #very temporary, will use objects soon
-def place_tile(x, y):
-
-    if(0 <= y < len(tiles_ground) and 0 <= x < len(tiles_ground[y]) and tiles_ground[y][x] == '0' and tiles2[y][x] == ' '):
-        tiles2[y][x] = 'f'
-        return True
-    return False
 
 
 # I have no idea why this actually works now lol DO NOT CHANGE
@@ -86,13 +47,13 @@ def window_to_game(mx, my):
 
 tile_map = []
 
-for y in range(len(tiles2)):
-    for x in range(len(tiles2[y])):
-        if tiles2[y][x] == 'f':
+for y in range(len(tiles.tiles2)):
+    for x in range(len(tiles.tiles2[y])):
+        if tiles.tiles2[y][x] == 'f':
             tile = FactoryTile(x, y)
             tile_map.append(tile)
 
-        if tiles2[y][x] == 't':
+        if tiles.tiles2[y][x] == 't':
             tile = ForestTile(x, y)
             tile_map.append(tile)
 
@@ -110,11 +71,12 @@ def main():
                 mx, my = pygame.mouse.get_pos()
                 mx, my = window_to_game(mx, my)
                 selected_x, selected_y = screen_to_iso(mx, my, ORIGIN_X, ORIGIN_Y)
-                place_tile(selected_x, selected_y)
+                print(selected_x, selected_y)
 
-                for tile in tile_map:
-                    if (tile.x == selected_x and tile.y == selected_y):
-                        tile.on_click()
+                for y in range(len(tiles.tile_map)):
+                    for x in range(len(tiles.tile_map[y])):
+                        if(tiles.tile_map[y][x].x == selected_x and tiles.tile_map[y][x].y == selected_y):
+                            tiles.tile_map[y][x].on_click()
 
     
 
@@ -122,58 +84,44 @@ def main():
 
         screen.fill((207, 236, 255))
         game_surface.fill((207, 236, 255))
-        for y in range(len(tiles_ground)):
-            for x in range(len(tiles_ground[y])):
 
-                if (tiles_ground[y][x] == '0'):
-                    ix, iy = tiles.coords_to_iso(x, y)
-                    game_surface.blit(assets.grass, (ix, iy))
-
-        # for y in range(len(tiles2)):
-        #     for x in range(len(tiles2[y])):
-
-        #         if (tiles2[y][x] == 'b'):
-        #             ix, iy = tiles.coords_to_iso(x, y)
-        #             game_surface.blit(assets.boundingbox, (ix, iy - HALF_W - SCALE_FACTOR))
-
-        #         if(tiles2[y][x] == 'f'):
-        #             ix, iy = tiles.coords_to_iso(x, y)
-        #             game_surface.blit(assets.factory, (ix, iy - HALF_W - SCALE_FACTOR))
-        #         if(tiles2[y][x] == 't'):
-        #             ix, iy = tiles.coords_to_iso(x, y)
-        #             game_surface.blit(assets.forest, (ix, iy - HALF_W - SCALE_FACTOR))
-
-        #         if(tiles2[y][x] == '@'):
-        #             ix, iy = tiles.coords_to_iso(x, y)
-        #             game_surface.blit(assets.mrpizza, (ix, iy - HALF_W - SCALE_FACTOR))
 
         #         if(tiles2[y][x] == '!'):
         #             ix, iy = tiles.coords_to_iso(x, y)
         #             game_surface.blit(assets.pyramid, (ix - (HALF_W * (2)) - SCALE_FACTOR * 2, iy - (HALF_H * (5)) - HALF_W + SCALE_FACTOR))
                 
-        for tile in tile_map:
-            tile.render(game_surface)
+        
+        #render all tiles
+        for y in range(len(tiles.ground_tile_map)):
+            for x in range(len(tiles.ground_tile_map[y])):
+                tiles.ground_tile_map[y][x].render(game_surface)
+
+        for y in range(len(tiles.tile_map)):
+            for x in range(len(tiles.tile_map[y])):
+                if(tiles.tile_map[y][x] != None):
+                    tiles.tile_map[y][x].render(game_surface)
 
 
+        # converting mouse coordinates - kind of weird (somehow works) do not change
         win_w, win_h = screen.get_size()
         scale = min(win_w / WIDTH, win_h / HEIGHT)
         scaled_w, scaled_h = int(WIDTH * scale), int(HEIGHT * scale)
         x = (win_w - scaled_w) // 2
         y = (win_h - scaled_h) // 2
-
         mx, my = pygame.mouse.get_pos()
-
         mx = (mx - x) / scale
         my = (my - y) / scale
+        # converting mouse coordinates
 
+
+        #selector
         imx, imy = screen_to_iso(mx, my, ORIGIN_X, ORIGIN_Y)
-        if(0 <= imy < len(tiles_ground) and 0 <= imx < len(tiles_ground[imy]) and tiles_ground[imy][imx] != ' '):
+        if(0 <= imy < len(tiles.tiles_ground) and 0 <= imx < len(tiles.tiles_ground[imy]) and tiles.tiles_ground[imy][imx] != ' '):
             game_surface.blit(assets.selector, tiles.coords_to_iso(imx, imy))
-
-        #screen.fill((0, 0, 0))  # black bars
+        #selector
+        
+        #general rendering
         screen.blit(pygame.transform.smoothscale(game_surface, (scaled_w, scaled_h)), (x, y))
-
-        #screen.blit(game_surface, (0, 0))
         pygame.display.update()
         clock.tick(30)
 
--- tiles.py
+++ tiles.py
@@ -8,6 +8,50 @@ def coords_to_iso(x, y):
 
     return config.ORIGIN_X + iso_x, config.ORIGIN_Y + iso_y
 
+tiles_ground = [[' ', ' ', ' ', ' ', '0', '0', '0', '0', ' ', ' ', '0', '0', '0', ' ', ' '],
+         [' ', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', ' ', ' '],
+         [' ', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', ' '],
+         ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', ' '],
+         ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', ' ', ' '],
+         ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', ' '],
+         [' ', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', ' '],
+         ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+         ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+         ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+         ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+         [' ', '0', '0', '0', '0', '0', '0', '0', '0', '0', ' ', '0', '0', '0', ' '],
+         [' ', '0', '0', '0', ' ', ' ', '0', '0', '0', '0', ' ', '0', '0', '0', ' '],
+         [' ', ' ', '0', '0', ' ', '0', '0', '0', '0', '0', ' ', '0', '0', '0', ' '],
+         [' ', ' ', ' ', ' ', ' ', ' ', ' ', '0', '0', ' ', ' ', ' ', ' ', ' ', ' ']]
+
+tiles2 = [[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+         [' ', ' ', '!', '~', '~', 't', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+         [' ', ' ', '~', '~', '~', 't', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+         [' ', ' ', '~', '~', '~', ' ', ' ', ' ', ' ', 't', ' ', ' ', ' ', ' ', ' '],
+         [' ', ' ', '@', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+         [' ', ' ', ' ', ' ', 'f', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+         [' ', ' ', ' ', ' ', ' ', 't', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 't', ' ', ' ', ' ', ' ', ' '],
+         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']]
+
+tile_map = []
+ground_tile_map = []
+
+def place_tile(x, y, tile):
+
+    if(0 <= y < len(tile_map) and 0 <= x < len(tile_map[y]) and type(ground_tile_map[y][x]) is GrassTile and type(tile_map[y][x]) is AirTile):
+        print(tile_map[y][x])
+        print(ground_tile_map[y][x])
+        tile_map[y][x] = tile(x, y)
+        return True
+    return False
+
 class Tile:
     def __init__(self, x, y):
         self.image = assets.grass
@@ -19,14 +63,26 @@ class Tile:
         ix, iy = coords_to_iso(self.x, self.y)
         surface.blit(self.image, (ix, iy - config.HALF_W - config.SCALE_FACTOR))
 
-        #empty
-
     def update(self):
         pass
 
     def on_click(self):
         pass
 
+    def place_tile(self):
+        pass
+
+class GrassTile(Tile):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+
+    def render(self, surface):
+
+        ix, iy = coords_to_iso(self.x, self.y)
+        surface.blit(self.image, (ix, iy))
+
+    # def on_click(self):
+    #     place_tile(self.x, self.y, FactoryTile)
 
 class FactoryTile(Tile):
     def __init__(self, x, y):
@@ -40,3 +96,35 @@ class ForestTile(Tile):
     def __init__(self, x, y):
         super().__init__(x, y)
         self.image = assets.forest
+
+class AirTile(Tile):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+
+    def render(self, surface):
+        pass
+
+    def on_click(self):
+        place_tile(self.x, self.y, FactoryTile)
+
+
+for y in range(len(tiles2)):
+    tile_row = []
+    for x in range(len(tiles2[y])):
+        if tiles2[y][x] == 'f':
+            tile_row.append(FactoryTile(x, y))
+        elif tiles2[y][x] == 't':
+            tile_row.append(ForestTile(x, y))
+        else:
+            tile_row.append(AirTile(x, y))
+    tile_map.append(tile_row)
+
+for y in range(len(tiles_ground)):
+    tile_row = []
+    for x in range(len(tiles_ground[y])):
+        if tiles_ground[y][x] == '0':
+            tile_row.append(GrassTile(x, y))
+        else:
+            tile_row.append(AirTile(x, y))
+    ground_tile_map.append(tile_row)
+
