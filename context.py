269ea1f19380b8c8e5bc269d16c4c458bb4bc4db
Talon Couture <talon.m.couture@gmail.com>
2026-02-12 16:00:38 -0500

    Increased difficulty for pyramid

--- context.py
+++ context.py
@@ -5,8 +5,8 @@ class GameContext:
         self.selected_item = None
         self.credits = 10
         self.bricks = 0
-        #self.required_bricks = [0, 2, 10, 20, 30, 40]
-        self.required_bricks = [0, 1, 2, 3, 4, 5]
+        self.required_bricks = [0, 2, 10, 20, 30, 40]
+        #self.required_bricks = [0, 1, 2, 3, 4, 5]
         self.pyramid_stage = 0
         self.built = False
         self.turn = 1
